f151a79d943ea6c185e28cc31d89caf1600db9a7
gouthamsivakumar1 <gouthamsivakumar1@gmail.com>
2019-04-08 20:55:42 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dementiacare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+ import android.graphics.drawable.GradientDrawable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
