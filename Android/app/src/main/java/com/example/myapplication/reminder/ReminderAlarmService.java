be826a39e535491bc15c712595625095760c9269
goutham sivakumar <gouthamsivakumar1@gmail.com>
2019-05-05 21:32:30 +0530

    updated reminder with cancel, notification etc

--- Android/app/src/main/java/com/example/myapplication/reminder/ReminderAlarmService.java
+++ Android/app/src/main/java/com/example/myapplication/reminder/ReminderAlarmService.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 
@@ -21,7 +22,6 @@ public class ReminderAlarmService extends IntentService {
 
     private static final int NOTIFICATION_ID = 42;
 
-    Cursor cursor;
     //This is a deep link intent, and needs the task stack
     public static PendingIntent getReminderPendingIntent(Context context, Uri uri) {
         Intent action = new Intent(context, ReminderAlarmService.class);
@@ -47,10 +47,7 @@ public class ReminderAlarmService extends IntentService {
                 .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
         //Grab the task description
-        if(uri != null){
-            cursor = getContentResolver().query(uri, null, null, null, null);
-        }
-
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
         String description = "";
         try {
             if (cursor != null && cursor.moveToFirst()) {
@@ -67,6 +64,8 @@ public class ReminderAlarmService extends IntentService {
                 .setContentText(description)
                 .setSmallIcon(R.drawable.ic_add_alert_black_24dp)
                 .setContentIntent(operation)
+                .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
                 .setAutoCancel(true)
                 .build();
 
